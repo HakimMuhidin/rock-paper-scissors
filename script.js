33c859a72dd42c8b2cb3ed00cf760e450038e750
abdihakim <130135387+HakimMuhidin@users.noreply.github.com>
2023-11-06 10:27:41 +0100

    function that plays one round

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 // create an funtion that get random choise from three alternatives
 
+
+
 function getComputerChoice(){
     choises = ["rock", "paper", "scissors"];
-    computerSelection = choises[Math.floor(Math.random()* choises.length)]
-    return computerSelection
+    return choises[Math.floor(Math.random()* choises.length)]
+
 }
 
 console.log(getComputerChoice)
+// create a funtion that plays one round of rock paper and scissors
+
+function playRound(playerSelection, computerSelection){
+   
+    if(playerSelection === "rock" && computerSelection ==="paper" ||
+       playerSelection === "paper" && computerSelection ==="scissors"||
+       playerSelection === "scissors" && computerSelection ==="rock"){
+        return `you lose ${computerSelection} beats ${playerSelection}`
+       }else if(playerSelection == computerSelection){
+        return "tie"
+       }else{
+        return `you win ${playerSelection} beats ${computerSelection}`
+       }
+}
 
 
+const playerSelection = prompt("rock paper or scissors").toUpperCase()
 
-
-const playerSelection = "rock";
 const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
