624182ef739106cc1ac8beebcb77ce883a02991f
abdihakim <130135387+HakimMuhidin@users.noreply.github.com>
2024-07-03 09:33:33 +0200

    come back to this to finsh it inshaa Allah

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// create an funtion that get random choise from three alternatives
-
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
 
 
 function getComputerChoice(){
@@ -9,25 +5,12 @@ function getComputerChoice(){
     return choices[Math.floor(Math.random()* choices.length)]
 }
 
-// create a funtion that plays one round of rock paper and scissors
-
-function playRound(playerSelection, computerSelection){
-   
-    if(playerSelection === "rock" && computerSelection ==="paper" ||
-       playerSelection === "paper" && computerSelection ==="scissors"||
-       playerSelection === "scissors" && computerSelection ==="rock"){
-        return `you lose ${computerSelection} beats ${playerSelection}`
-       }else if(playerSelection === computerSelection){
-        return "tie"
-       }else{
-        return `you win ${playerSelection} beats ${computerSelection}`
-       }
-}
 
 
 
-// const playerSelection = prompt("rock paper or scissors").toLowerCase()
 
 
 
-console.log(playRound(playerSelection, computerSelection));
+
+//  const playerSelection = prompt("rock paper or scissors").toLowerCase()
+const computerSelection = getComputerChoice();
