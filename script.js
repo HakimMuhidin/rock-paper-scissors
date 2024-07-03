ec270cc74462ca5a530af9fb372b081739ffc0fc
abdihakim <130135387+HakimMuhidin@users.noreply.github.com>
2024-07-03 10:29:40 +0200

    i have managed to play one round

--- script.js
+++ script.js
@@ -5,12 +5,29 @@ function getComputerChoice(){
     return choices[Math.floor(Math.random()* choices.length)]
 }
 
+function getHumanChoice(){
+    let getChoice;
+    getChoice = prompt("rock papper or scissors")
+    return getChoice;
+}
 
+let humanScore = 0
+let computerScore = 0;
 
+function playround(humanChoice, computerChoice){
+    if(humanChoice === "paper" && computerChoice === "rock" ||
+       humanChoice ==="rock" && computerChoice ==="scissors" ||
+        humanChoice ==="scissors" && computerChoice === "paper"
+    ){
 
+        return "human wins" 
+    }else{
+        return "computer wins"
+    }
+}
 
-
-
-
-//  const playerSelection = prompt("rock paper or scissors").toLowerCase()
+const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
+
+
+playround(humanSelection, computerSelection);
